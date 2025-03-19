454bbc3cdf29a945c251f60524a3004c2c81a76a
JEANMARIO <jean.hernandez@sigsa.com.mx>
2025-03-18 19:41:33 -0600

    avance hasta los responsive

---
+++ index.js
@@ -0,0 +1,27 @@
+window.addEventListener("click", function(event){
+    if(document.getElementById('navbar-botton').contains(event.target)){
+        showHideDropwnMenu();
+    }
+
+    if(!document.getElementById('navbar-botton').contains(event.target) && document.getElementById("myDropDownMenu").style.display == "block"){
+        hideDropwnMenu()
+    }
+})
+
+function showHideDropwnMenu() {
+    var miniMenu = document.getElementById("myDropDownMenu");
+    if(miniMenu.style.display == ""){
+        miniMenu.style.display = "flex";
+        return true
+    }
+    else{
+        miniMenu.style.display = "";
+    }
+}
+
+function hideDropwnMenu(){
+    document.getElementById("myDropDownMenu").style.display = ""
+}
+
+
+
